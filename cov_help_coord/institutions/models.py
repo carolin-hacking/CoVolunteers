19596b394e9988c713f7c2e4785849104b239506
Carolin <carolin.hecking@tum.de>
2020-03-20 18:48:31 +0100

    add many to many helper field

--- cov_help_coord/institutions/models.py
+++ cov_help_coord/institutions/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from helpers.models import Helper
 
 class Institution(models.Model):
     SUPERMARKET = 'SUP'
@@ -20,3 +21,4 @@ class Institution(models.Model):
     description = models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User, related_name="companies", on_delete=models.CASCADE, null=True)
+    helpers = models.ManyToManyField(Helper)
