6391696d1e80fdf533b1c3246e4a3e4e26aaf271
Maximilian Deichmann <m.deichmann@tum.de>
2020-03-21 19:11:28 +0100

    restructure

---
+++ backend/cov_help_coord/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('helpers.urls')),
+    path('', include('accounts.urls')),
+    path('', include('institutions.urls')),
+    path('admin/', admin.site.urls),
+]
