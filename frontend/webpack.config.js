34812b0d47f057933a255626f065c27c623c701f
Carolin <carolin.hecking@tum.de>
2020-03-24 01:06:52 +0100

    ad

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -64,6 +64,7 @@ module.exports = {
         changeOrigin: true
       },
     },
+    hot: true,
     onListening: function(server) {
       const port = server.listeningApp.address().port;
       console.log('Listening on port:', port);
